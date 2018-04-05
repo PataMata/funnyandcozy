4a40e6f95c302869bbe9ad15c97aa2c4807f3673
PataMata <skjella@hotmail.no>
2018-04-05 19:17:50 +0200

    MySQL Test
    Testing mySQL connection

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+
+$servername = "den1.mysql6.gear.host";
+$username = "funnyandcozy1";
+$password = "Tl9NR_5u_30v";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
