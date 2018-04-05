dfc8ae47f0ea0445fbec599508d5e8cadc1eb6e3
PataMata <skjella@hotmail.no>
2018-04-05 19:12:19 +0200

    Test 2
    Test 2

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<title>Funny & Cozy</title>
 </head>
 <body>
 
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
+<h1>Markus suger</h1>
 
 </body>
 </html>
