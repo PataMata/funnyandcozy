1581970a0df47f5707f80e8e402960608adad2c4
PataMata <skjella@hotmail.no>
2018-04-06 14:05:33 +0200

    t
    t

--- connection.php
+++ connection.php
@@ -5,11 +5,11 @@ $username = "funnyandcozy1";
 $password = "Tl9NR_5u_30v";
 
 global $conn = mysql_connect($servername, $username, $password) or die(mysql_error());
-mysql_select_db("funnyandcozy1") or die(mysql_error());
+//mysql_select_db("funnyandcozy1") or die(mysql_error());
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo("Connection failed: " . $conn->connect_error);
 } else {
 	echo "DB Connection succesfull";
 }
