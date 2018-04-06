e7cae0c7c9920e29c6ccf0156eedd11759223561
PataMata <skjella@hotmail.no>
2018-04-06 14:00:44 +0200

    Selected db

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ $username = "funnyandcozy1";
 $password = "Tl9NR_5u_30v";
 
 $conn = mysql_connect($servername, $username, $password) or die(mysql_error());
-mysql_select_db("Database_Name") or die(mysql_error());
+mysql_select_db("funnyandcozy1") or die(mysql_error());
 
 // Check connection
 if ($conn->connect_error) {
