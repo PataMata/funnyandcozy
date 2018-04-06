d2b207d074fdf732dd821b4d759a6c121d0ff677
PataMata <skjella@hotmail.no>
2018-04-06 14:03:27 +0200

    g
    g

--- connection.php
+++ connection.php
@@ -4,11 +4,13 @@ $servername = "den1.mysql6.gear.host";
 $username = "funnyandcozy1";
 $password = "Tl9NR_5u_30v";
 
-$conn = mysql_connect($servername, $username, $password) or die(mysql_error());
+global $conn = mysql_connect($servername, $username, $password) or die(mysql_error());
 mysql_select_db("funnyandcozy1") or die(mysql_error());
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+} else {
+	echo "DB Connection succesfull";
+}
 ?> 
